cef20f877fb1dbc005f236a1b4c0b3ae716d26ed
Mujtaba1i <unmujt@gmail.com>
2025-12-11 13:35:00 +0300

    'R' in CRUD is DONE

--- GameSite-CRUD/controllers/profile.js
+++ GameSite-CRUD/controllers/profile.js
@@ -8,13 +8,25 @@ const Users = require('../models/user')
 // GET ============================================================================================
 
 router.get('/', async(req,res)=>{
-    res.render('profile/index.ejs')
+    // find all the games that the logged in user "OWNE"
+    const games = await Games.find({owner: req.session.user._id}).populate('owner')
+    res.render('profile/index.ejs', { games })
 })
 
 router.get('/new', async(req,res)=>{
     res.render('profile/new.ejs')
 })
 
+router.get('/:id', async(req,res)=>{
+    const game = await Games.findById(req.params.id).populate('owner')
+    res.render('profile/show.ejs', { game })
+})
+
+router.get('/:id/edit',async(req,res)=>{
+    const game = await Games.findById(req.params.id)
+    res.render('profile/edit.ejs', { game })
+
+})
 
 // POST ===========================================================================================
 
@@ -29,6 +41,20 @@ router.post('/', async(req,res)=>{
     }
 
 })
+
+// UPDATE (PUT) ===================================================================================
+
+router.put('/:id', async(req,res)=>{
+       try {
+        req.body.owner = req.session.user._id
+        const updatedGame = await Games.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect('/profile')        
+    } catch (err) {
+        console.error('Ran into and error: '+err)
+        res.redirect('/profile')
+    } 
+})
+
 
 // exports ========================================================================================
 
