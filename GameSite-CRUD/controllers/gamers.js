831c7e775513fddea5cf8c5480605969572f28e1
Mujtaba1i <unmujt@gmail.com>
2025-12-13 13:45:27 +0300

    done with the show page for the public accounts and the private accounts that has public games and made sure to only display the public games with their status

--- GameSite-CRUD/controllers/gamers.js
+++ GameSite-CRUD/controllers/gamers.js
@@ -49,6 +49,26 @@ router.get('/', async(req,res)=>{
     }
 })
 
+router.get('/:id',async(req,res)=>{
+    try {
+        const user = await Users.findById(req.params.id)
+        if (user.accountState === 'Private'){
+            user.username = 'Anonymous'
+        }
+        const games = await Games.find({owner: user._id, gameState: 'Public'})
+        // get the total money spent on all games that are set to public
+        let totalMoneySpent = 0 
+        games.forEach(game =>{
+            totalMoneySpent += game.price
+        }) 
+        res.render('gamers/show.ejs', { user, games, totalMoneySpent })
+    } 
+    catch (err) {
+        console.error('Ran into and error: '+err)
+        res.redirect('/')  
+    }
+})
+
 // POST ===========================================================================================
 
 
