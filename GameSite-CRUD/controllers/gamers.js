ed7cacb81c46a103646f91c333a620a9511a6c6c
Mujtaba1i <unmujt@gmail.com>
2025-12-13 13:15:21 +0300

    filtered the account and displaying all public accounts and private accounts that has public games as Anonymous #

--- GameSite-CRUD/controllers/gamers.js
+++ GameSite-CRUD/controllers/gamers.js
@@ -3,11 +3,50 @@
 const express = require('express')
 const router = express.Router()
 const Users = require('../models/user')
+const Games = require('../models/game')
 
 // GET ============================================================================================
 
 router.get('/', async(req,res)=>{
-    res.render('gamers/index.ejs')
+    try{
+        
+        let userAccountsToDisplay = []
+        let anonymousCounter = 1
+        // finds all users
+        const users = await Users.find()
+        
+        // loop through all
+        for (let user of users) {
+            if (user.accountState === 'Public') { // if account is public add it
+                const userCopy = {
+                    _id: user._id,
+                    username: user.username,
+                    accountState: user.accountState
+                }
+                userAccountsToDisplay.push(userCopy)
+            } 
+            else {
+                // checks if there is any private accounts that has public games
+                const publicGames = await Games.find({owner: user._id, gameState: 'Public'})
+                if (publicGames.length > 0) {
+                    // if yes add and change display name to Anonymous
+                    const userCopy = {
+                        _id: user._id,
+                        username: `Anonymous ${anonymousCounter}`,
+                        accountState: user.accountState
+                    }
+                    userAccountsToDisplay.push(userCopy)
+                    anonymousCounter++
+                }
+            }
+        }
+        console.log(userAccountsToDisplay)
+        res.render('gamers/index.ejs', { users: userAccountsToDisplay })
+    }
+    catch(err){
+        console.error('Ran into and error: '+err)
+        res.redirect('/')
+    }
 })
 
 // POST ===========================================================================================
